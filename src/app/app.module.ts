de3f842360dbc5f5d59e487f5c4a836697e8ca2c
Sampath Kumar Ajjapaga <sampathkumar.ajjapaga@gmail.com>
2020-05-06 05:19:35 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { AllTasksComponent } from './sections/all-tasks/all-tasks.component';
+import { MyTasksComponent } from './sections/my-tasks/my-tasks.component';
+import { MyGroupTasksComponent } from './sections/my-group-tasks/my-group-tasks.component';
+import { FindACaseComponent } from './sections/find-a-case/find-a-case.component';
+import { HistoricalCasesComponent } from './sections/historical-cases/historical-cases.component';
+import { ActiveCasesComponent } from './sections/active-cases/active-cases.component';
+import { MyDashboardComponent } from './sections/my-dashboard/my-dashboard.component';
+import { HomeComponent } from './sections/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponent,
+    MenuComponent,
+    SideBarComponent,
+    AllTasksComponent,
+    MyTasksComponent,
+    MyGroupTasksComponent,
+    FindACaseComponent,
+    HistoricalCasesComponent,
+    ActiveCasesComponent,
+    MyDashboardComponent,
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
